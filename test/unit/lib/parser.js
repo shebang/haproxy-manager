4c4575f74b3654fe6ef9adc26d5cdf276be6c202
Arndt Lueckerath <a.lueckerath@shebang.org>
2018-06-01 19:13:53 +0200

    improved parsing

--- test/unit/lib/parser.js
+++ test/unit/lib/parser.js
@@ -87,7 +87,7 @@ describe('Parser', () => {
             expect(result[0]).to.be.an.object();
         });
 
-        it('should return an array of objects with param startOffset and unknwo data on line 1', () => {
+        it('should return an array of objects with param startOffset and output format on line 1', () => {
 
             const data = '1\n# a,b\n1,2';
             const result = parser.parseHaproxyOutput(data, ',', 1);
@@ -103,6 +103,72 @@ describe('Parser', () => {
             expect(result).to.be.equal([{ a: '1', b: '2' }]);
             expect(result).to.be.an.array();
             expect(result[0]).to.be.an.object();
+        });
+    });
+
+    experiment('_deleteLastCharFromStrings', () => {
+
+        it('should remove the last character from an array of strings', () => {
+
+            const data = ['1,','2','1,2,'];
+            const result = parser._deleteLastCharFromStrings(data, ',');
+            expect(result).to.be.an.array();
+            expect(result).to.be.equal(['1','2','1,2']);
+        });
+    });
+
+    experiment('_parseRawData', () => {
+
+        it('should return fields and data lines with param startOffset', () => {
+
+            const data = '1\n# a b\n1 2\n3 4';
+            const result = parser._parseRawData(data, ' ', 1);
+            expect(result.fields).to.be.an.array();
+            expect(result.fields).to.be.equal(['a','b']);
+            expect(result.data).to.be.an.array();
+            expect(result.data).to.be.equal(['1 2','3 4']);
+        });
+
+        it('should return fields and data lines without param startOffset', () => {
+
+            const data = '# a b\n1 2\n3 4';
+            const result = parser._parseRawData(data, ' ');
+            expect(result.fields).to.be.an.array();
+            expect(result.fields).to.be.equal(['a','b']);
+            expect(result.data).to.be.an.array();
+            expect(result.data).to.be.equal(['1 2','3 4']);
+        });
+
+        it('should remove last character \',\' of each line', () => {
+
+            const data = '# a,b,\n1,2';
+            const result = parser._parseRawData(data, ',');
+            expect(result.fields).to.be.an.array();
+            expect(result.fields).to.be.equal(['a','b']);
+            expect(result.data).to.be.an.array();
+            expect(result.data).to.be.equal(['1,2']);
+        });
+    });
+
+    experiment('parseServersState', () => {
+
+        it('should return servers state', () => {
+
+            let data;
+            data += '1\n';
+            data += '# be_id be_name srv_id srv_name srv_addr srv_op_state\n';
+            data += '3 servers 1 i-07dcd59ded437b0e8 10.11.45.226 2\n';
+            data += '3 servers 2 i-0c16773ceb4b2d619 10.11.26.229 2\n';
+            data += '3 servers 3 i-0724abfb014284b53 10.11.5.120 2\n';
+            data += '4 websockets 1 i-07dcd59ded437b0e8 10.11.45.226 2\n';
+            data += '4 websockets 2 i-0c16773ceb4b2d619 10.11.26.229 2\n';
+            data += '4 websockets 3 i-0724abfb014284b53 10.11.5.120 2\n';
+            const result = parser.parseServersState(data, ' ');
+            console.log(result);
+            expect(result.servers).to.be.an.object();
+            expect(result.servers.be_id).to.be.equal('3');
+            expect(result.servers.servers[0].srv_id).to.be.equal('1');
+            expect(result.servers.servers[0].srv_name).to.be.equal('i-07dcd59ded437b0e8');
         });
     });
 });
